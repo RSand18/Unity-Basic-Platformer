b37befe06450e9c304ffed0bd996eb8a1bf8a7b3
RSand18 <43037672+RSand18@users.noreply.github.com>
2018-12-17 22:50:16 -0600

    Imported Assets, Created Grounds and Spawn Manager

---
+++ Assets/Scripts/Reset.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Reset : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
